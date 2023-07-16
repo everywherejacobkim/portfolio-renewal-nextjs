c4b0678ed82b1d34708f0558c37f1fb0ebbded07
Jacob Namhyung Kim <87889917+everywherejacobkim@users.noreply.github.com>
2023-07-15 20:12:41 -0700

    Chore: Fix rendering error caused by unused import

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,4 @@
 import Image from 'next/image';
-import PageTransition from './components/transition';
 import Header from './components/header';
 import Footer from './components/footer';
 import MainFigure from './components/mainFigure/MainFigure';
