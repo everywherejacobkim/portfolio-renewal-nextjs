3c0235f2527bec85b3b7c350f89c465f1e42b812
Jacob Namhyung Kim <87889917+everywherejacobkim@users.noreply.github.com>
2023-07-15 20:40:50 -0700

    Fix: Replace render format to resolve building error

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,6 @@ import '@/styles/globals.css';
 import { AnimatePresence, motion } from 'framer-motion';
 import type { AppProps } from 'next/app';
 import { useRouter } from "next/router";
-import { transitionVariants } from './components/transition';
 
 export default function App({ Component, pageProps }: AppProps) {
   const router = useRouter();
@@ -12,12 +11,22 @@ export default function App({ Component, pageProps }: AppProps) {
           onExitComplete={() => window.scrollTo(0, 0)}>
             <motion.div
               key={router.route}
-              initial= "initial"
-              animate= "animate"
-              exit= "exit"
-              variants={transitionVariants}
+              initial={{
+                clipPath: "polygon(0 0, 0 0, 0 100%, 0% 100%)",
+                opacity: 0,
+              }}
+              animate={{
+                clipPath: "polygon(0 0, 100% 0, 100% 100%, 0 100%)",
+                transition: { duration: 0.7, staggerChildren: 0.2 },
+                opacity: 1,
+              }}
+              exit={{
+                clipPath: "polygon(100% 0, 100% 0, 100% 100%, 100% 100%)",
+                transition: { duration: 0.5 },
+                opacity: 0,
+              }}
               transition={{
-                duration: 0.5,
+                duration: 0.25,
                 ease: [0.43, 0.13, 0.23, 0.96],
               }}
             >
