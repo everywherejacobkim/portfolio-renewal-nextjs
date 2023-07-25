19bd7a48d239dbaa06034c890e608c44993be30d
Jacob Namhyung Kim <87889917+everywherejacobkim@users.noreply.github.com>
2023-07-24 18:28:25 -0700

    Chore: revise typedText for header

--- src/pages/components/header/index.tsx
+++ src/pages/components/header/index.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import Link from "next/link";
 import { FaBars, FaTimes } from "react-icons/fa";
-import { TypeAnimation } from 'react-type-animation';
+import { TypeAnimation } from "react-type-animation";
 
 const Header: React.FC = () => {
   const [isExpanded, setIsExpanded] = useState(false);
@@ -9,19 +9,33 @@ const Header: React.FC = () => {
     setIsExpanded(!isExpanded);
   };
 
-  const typedText = ["Front End Developer",1000, "React Developer",1500, "TypeScript Developer",1000, "UI Developer",1000];
+  const typedText = [
+    "Front End Developer",
+    1000,
+    "React Developer",
+    1500,
+    "TypeScript Developer",
+    1000,
+    "Full Stack Developer",
+    1500,
+  ];
 
   return (
     <header className="bg-white p-2">
       <nav className="flex items-center justify-between max-w-7xl mx-auto pl-4 py-3">
-        <Link href="/" className="w-[305px] shadow-lg py-2 pl-6 md:px-6 rounded-sm bg-white ml-2 md:ml-8 mr-2">
+        <Link
+          href="/"
+          className="w-[305px] shadow-lg py-2 pl-6 md:px-6 rounded-sm bg-white ml-2 md:ml-8 mr-2"
+        >
           <TypeAnimation
             sequence={typedText}
             className="text-primary-200 text-xl md:text-2xl font-bold"
             wrapper="h2"
             repeat={Infinity}
           />
-          <p className="text-gray-900 text-xl md:text-2xl font-bold">Jacob Namhyung Kim</p>
+          <p className="text-gray-900 text-xl md:text-2xl font-bold">
+            Jacob Namhyung Kim
+          </p>
         </Link>
         <button className="block mx-6 md:hidden" onClick={toggleMenu}>
           {isExpanded ? (
@@ -41,7 +55,10 @@ const Header: React.FC = () => {
             </Link>
           </li>
           <li>
-            <Link href="/skills" className="px-2 py-1 block hover:text-gray-500">
+            <Link
+              href="/skills"
+              className="px-2 py-1 block hover:text-gray-500"
+            >
               Skills
             </Link>
           </li>
