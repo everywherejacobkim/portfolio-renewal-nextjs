5f7f38f93824b353bd9c435c0be5ba1871326649
Jacob Namhyung Kim <87889917+everywherejacobkim@users.noreply.github.com>
2023-10-04 17:31:50 -0700

    Style: make single project page responsive to mobile view

--- src/pages/projects/[id]/index.tsx
+++ src/pages/projects/[id]/index.tsx
@@ -9,9 +9,7 @@ import { FcIdea } from "react-icons/fc";
 import { MdLiveTv } from "react-icons/md";
 import { BsGithub } from "react-icons/bs";
 
-type Props = {};
-
-const index = (props: Props) => {
+const index = () => {
   // eslint-disable-next-line react-hooks/rules-of-hooks
   const router = useRouter();
   const { id } = router.query;
@@ -22,36 +20,36 @@ const index = (props: Props) => {
     <div className="bg-white">
       <Header />
       {selectedProject && (
-        <div className="pl-12 max-w-7xl mx-auto">
-          <div className="flex flex-row-reverse shadow-lg rounded-lg overflow-hidden my-2">
+        <div className="px-2 md:px-12 max-w-7xl mx-auto">
+          <div className="shadow-lg rounded-lg overflow-hidden my-2 md:flex md:flex-row-reverse">
             <Link href={selectedProject.link}>
-              <div>
+              <div className="w-full">
                 <Image
                   src={selectedProject.image}
-                  width={1000}
+                  width={600}
                   height={600}
                   alt={selectedProject.title}
                 />
               </div>
             </Link>
-            <div className="p-16">
+            <div className="p-4 md:p-16 w-full md:w-1/2">
               <h3 className="text-4xl font-semibold">
                 {selectedProject.title}
               </h3>
-              <p className="text-lg mt-1 font-medium text-primary-200">
+              <p className="text-xs md:text-lg mt-1 font-medium text-primary-200">
                 {selectedProject.stack}
               </p>
-              <div className="flex flex-col gap-4 my-8">
+              <div className="flex flex-col gap-4 my-4 md:my-8">
                 <div className="flex items-start gap-3">
-                  <FcIdea className="w-6 h-6" />
-                  <p className="w-3/4 text-lg font-medium text-gray-600">
+                  <FcIdea className="w-7 h-7" />
+                  <p className="w-full text-sm md:text-lg font-medium text-gray-600">
                     {selectedProject.description}
                   </p>
                 </div>
-                <div className="flex items-center gap-3 mt-4">
-                  <MdLiveTv className="w-6 h-6 animate-bounce" />
+                <div className="flex items-center gap-3 mt-2 md:mt-4">
+                  <MdLiveTv className="w-7 h-7 animate-bounce" />
                   <Link href={selectedProject.link}>
-                    <p className="font-medium text-primary-200 hover:text-primary-40">
+                    <p className="text-sm md:text-base font-medium text-primary-200 hover:text-primary-400">
                       {selectedProject.link}
                     </p>
                   </Link>
@@ -59,7 +57,7 @@ const index = (props: Props) => {
                 <div className="flex items-center gap-3">
                   <BsGithub className="w-6 h-6" />
                   <Link href={selectedProject.code}>
-                    <p className="font-medium text-primary-200 hover:text-primary-40">
+                    <p className="text-sm md:text-base font-medium text-primary-200 hover:text-primary-400">
                       {selectedProject.code}
                     </p>
                   </Link>
@@ -71,7 +69,7 @@ const index = (props: Props) => {
             <iframe
               src={selectedProject.pdf}
               width="100%"
-              height="900px"
+              height="800px"
               className="mt-4 rounded-lg"
             ></iframe>
           )}
